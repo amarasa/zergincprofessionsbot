5ced269f03179e690437a9027694c37b632b896c
Angelo <lpangm03@yahoo.com>
2021-07-04 11:41:08 -0400

    Added 2 required files

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 //const env = process.env;
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+const path = require('path');
 
 const PROFS_PREFIX = '.profs';
 const ENCH_PREFIX = '.ench ';
